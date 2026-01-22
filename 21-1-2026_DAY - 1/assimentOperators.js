f8df77d991229761558c5202f3d74f1337b03901
Aashish Jawalekr <158753294+Aashish7112004@users.noreply.github.com>
2026-01-22 19:00:47 +0530

    Delete 21-1-2026_DAY - 1 directory

--- 21-1-2026_DAY - 1/assimentOperators.js
+++
@@ -1,5 +0,0 @@
-console.log("Assignment Operators in JavaScript")
-console.log("Assignment operators are used to assign values to variables\n")
-console.log("List of Assignment Operators: =, +=, -=, *=, /=, %=, **=\n")
-
-let x = 10  
